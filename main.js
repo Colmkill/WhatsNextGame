ba2a50bfb8a0d530636a5b48f914caa8cda88c0a
Colmkill <60960023+Colmkill@users.noreply.github.com>
2026-09-08 06:21:52 +0930

    Refactor and enhance game initialization and themes

--- main.js
+++ main.js
@@ -216,6 +216,10 @@ const masterLevelPool = [
     [
         "letter_tile_0", "letter_tile_1", "letter_tile_2", 
         "letter_tile_3", "letter_tile_23", "letter_tile_11", "letter_tile_7"
+    ],
+    [
+        "letter_tile_5", "letter_tile_15", "letter_tile_18", 
+        "letter_tile_9", "letter_tile_23", "letter_tile_11", "letter_tile_7"
     ]
 ];
 
